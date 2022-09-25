c90e517e2803d67e4ca563d58915b7cb1792db4c
Jørgen Lund <jlu@dips.no>
2022-09-25 18:51:39 +0200

    Add code for hyperparameter search

---
+++ classifier_search.py
@@ -0,0 +1,154 @@
+"""
+PsychOrNot - Experiment 2
+
+"""
+
+from distutils.command.build import build
+from platform import architecture
+from typing import List, Tuple
+
+import itertools
+import argparse
+import logging
+import json
+
+import pandas as pd
+import numpy as np
+
+import sklearn.feature_extraction.text
+import sklearn.pipeline
+import sklearn.ensemble
+import sklearn.model_selection
+import sklearn.metrics
+import sklearn.svm
+
+import tensorflow as tf
+import tensorflow.keras as keras
+import keras_tuner
+import keras_tuner.tuners
+import keras_tuner_cv.inner_cv
+import keras_tuner_cv.utils
+
+def build_input_pipeline():
+    transform_steps = [('vectorize', sklearn.feature_extraction.text.CountVectorizer(
+        analyzer='char',
+        lowercase=False,
+        ngram_range=(2,2),
+        vocabulary=feature_names_for_ngram_range(
+            AMINO_ACIDS, (2,2))
+    ))]
+    transform_steps.append(('normalize', sklearn.feature_extraction.text.TfidfTransformer(use_idf=True)))
+    return sklearn.pipeline.Pipeline(transform_steps)
+
+def build_model(hp):
+    model = tf.keras.Sequential([
+        tf.keras.Input(shape=(400,))])
+    
+    for i in range(hp.Int("layers", 1, 3)):
+        model.add(tf.keras.layers.Dense(units=hp.Int(f"units_{i}", min_value=32, max_value=512, step=32), activation='relu'))
+    
+    # Add the prediction head:
+    model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer=tf.keras.optimizers.RMSprop(),
+                loss=tf.keras.losses.BinaryCrossentropy(),
+                metrics=['accuracy'])
+    
+    return model
+
+# AMINO_ACIDS is the vocabulary used to construct the features.
+# These correspond to the FASTA format symbols, except for
+# pyrrolysine and selenocysteine (which do not appear in the dataset)
+# and special characters, which are ignored.
+AMINO_ACIDS = "AGSTNQVILMFYWHPKREDC"
+
+# LABEL_MAP is the mapping from class names to class indices.
+LABEL_MAP = {'Thermo': 0, 'Psychro': 1}
+
+# RANDOM_SEED is the initial RNG state used to divide the dataset into training-test sets
+# and cross-validation folds.
+RANDOM_SEED = 1
+
+# HOLDOUT_SET_SIZE is the ratio of points to set aside for the final holdout test set (unused in this experiment.)
+# In our experiment, we set aside 10% of the points for the holdout set.
+HOLDOUT_SET_SIZE = 0.1
+
+# CROSSVALIDATION_FOLDS is the number of folds to use.
+CROSSVALIDATION_FOLDS = 10
+
+# EXPERIMENT_METRICS are the accuracy metrics shown when the experiment is finished (see representation-results.csv)
+EXPERIMENT_METRICS = ['train-accuracy', 'validation-accuracy']
+
+# SORT_METRIC is the accuracy metric to rank results by (only used for displaying results)
+SORT_METRIC = 'validation-accuracy'
+
+
+# To ensure features appear in the same order,
+# we provide a helper to compute the vocabulary for the n-gram representation:
+
+
+def feature_names_for_ngram_range(vocabulary: str, ngram_range: Tuple[int, int]) -> List[str]:
+    """
+    feature_names_for_ngram_range returns the set of possible n-grams
+    for the given vocabulary and n in (ngram_range)
+
+    >>> feature_names_for_ngram_range("ab", 1, 2)
+    ['a', 'b', 'aa', 'ab', 'ba', 'bb']
+    """
+    features = []
+    for n in range(ngram_range[0], ngram_range[1]+1):
+        features += [''.join(c)
+                     for c in itertools.product(vocabulary, repeat=n)]
+    return features
+
+def main(args):
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    dataset = pd.read_csv(args.datapath, names=(
+        "Classification", "Label", "Sequence"))
+
+    X, y = dataset['Sequence'], dataset['Label'].map(LABEL_MAP).to_numpy()
+    X_cv, X_holdout, y_cv, y_holdout = sklearn.model_selection.train_test_split(
+        X, y, shuffle=True, test_size=HOLDOUT_SET_SIZE, random_state=RANDOM_SEED)
+    kfold = sklearn.model_selection.StratifiedKFold(
+        n_splits=CROSSVALIDATION_FOLDS, shuffle=True, random_state=RANDOM_SEED)
+
+    X_train, X_validation, y_train, y_validation = sklearn.model_selection.train_test_split(
+        X_cv, y_cv, shuffle=True, test_size=0.2
+    )
+
+    pipeline = build_input_pipeline()
+    X_train_t = pipeline.fit_transform(X_train).toarray()
+    X_validation_t = pipeline.transform(X_validation).toarray()
+
+    parameter_tuner = keras_tuner_cv.inner_cv.inner_cv(keras_tuner.tuners.RandomSearch)(
+        build_model,
+        kfold,
+        objective="val_accuracy",
+        max_trials=3,
+        executions_per_trial=2,
+        overwrite=True,
+        directory="classifier_search",
+        project_name="psychornot",
+        save_history=True,
+        save_output=True,
+    )
+
+    parameter_tuner.search(X_train_t, y_train, epochs=10, validation_data=(X_validation_t, y_validation))
+
+    df = keras_tuner_cv.utils.pd_inner_cv_get_result(parameter_tuner)
+    print(df.head())
+    df.to_csv(args.results)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Compare classifier architectures for the PsychOrNot classifier.")
+    parser.add_argument("--datapath", default="dataset/database-pdb.csv",
+                        help="The path to the .CSV with the experiment data.")
+    parser.add_argument("--results", default="classification-results.csv",
+                        help="Where to save the results of the representation comparison.")
+    parser.add_argument("--verbose", action="store_true",
+                        help="Log results to the console.")
+    args = parser.parse_args()
+    main(args)
